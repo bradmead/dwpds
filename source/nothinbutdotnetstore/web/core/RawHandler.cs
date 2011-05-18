599260c32594b05d2b6e87880f610694d774d497
Brad Mead <bradmead@gmail.com>
2011-05-18 10:06:09 -0700

    Pushing new changes

--- source/nothinbutdotnetstore/web/core/RawHandler.cs
+++ source/nothinbutdotnetstore/web/core/RawHandler.cs
@@ -5,14 +5,24 @@ namespace nothinbutdotnetstore.web.core
 {
   public class RawHandler : IHttpHandler
   {
+    private readonly IProcessIncomingRequests _processIncomingRequests;
+    private readonly ICreateRequests _createRequests;
+
+    public RawHandler(IProcessIncomingRequests processIncomingRequests, ICreateRequests createRequests)
+    {
+      _processIncomingRequests = processIncomingRequests;
+      _createRequests = createRequests;
+    }
+
     public void ProcessRequest(HttpContext context)
     {
-      throw new NotImplementedException();
+      object requestFrom = _createRequests.create_request_from(context);
+      _processIncomingRequests.process(requestFrom);
     }
 
     public bool IsReusable
     {
-      get { throw new NotImplementedException(); }
+      get { return true; }
     }
   }
 }
